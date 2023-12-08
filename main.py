4de7baa568cf997e6763fbf74f77e0f17ae5074a
Vedad Misirlic <vedad.misirlic@student.tugraz.at>
2023-12-08 13:02:39 +0100

    Added imports to the program. Prepared everything for initial initialisation of data

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import numpy as np
+import pandas as pd
+import tqdm as tqdm
+import torch as torch
+import sklearn
+import nltk
+import matplotlib as plt
+import seaborn as sns
+
+
+
 print("Hello")
