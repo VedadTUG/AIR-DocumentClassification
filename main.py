05401605dc8e634c8833191a51220cf15e8e3617
Vedad Misirlic <vedad.misirlic@student.tugraz.at>
2023-12-14 14:19:06 +0100

    added labels as per file descriptors

--- main.py
+++ main.py
@@ -16,4 +16,11 @@ def read_data(path):
     return list_of_entries
 
 path = 'data/alt.atheism'
-print(len(read_data(path)))
+
+
+labels = [
+    'alt_atheism', 'comp_graphics', 'comp_ms_windows_misc', 'comp_ibm', 'comp_mac', 'comp_windows', 'misc_forsale',
+    'rec_autos', 'rec_motorcycles', 'rec_sport_basketball', 'roc_sport_hockey', 'sci_crypt', 'sci_electronics',
+    'sci_med', 'sci_space', 'soc_religion_christian', 'politics_guns', 'politics_mideast', 'politics_misc,', 'religion_misc'
+        ]
+
